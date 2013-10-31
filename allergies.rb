6540339c55c7c80745917adb9af875f61f712b7d
Michael Pavling <pavling@gmail.com>
2013-10-31 16:15:49 +0000

    passing test_no_allergies_at_all

---
+++ allergies.rb
@@ -0,0 +1,24 @@
+class Allergies
+
+  ALLERGIES = {
+    1 => 'eggs',
+    2 => 'peanuts',
+    4 => 'shellfish',
+    8 => 'strawberries',
+    16 => 'tomatoes',
+    32 => 'chocolate',
+    64 => 'pollen',
+    128 => 'cats',
+  }
+
+  def initialize(score)
+    @score = score
+  end
+
+  def list
+    ALLERGIES.keys.select { |key| key & @score == key }.map { |key| ALLERGIES.fetch(key)}
+  end
+
+
+
+end
